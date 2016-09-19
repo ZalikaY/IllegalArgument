decdb9b5f356acdc868c3c25a01c8bf190ea9a5b
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-19 16:12:58 -0400

    Second draft

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,8 +2,19 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String [] names = {"Armani","Yazmin", "Tyler","Nicole","Daniel","David","Dominique E", "Elle","Clark","Kevin","Jennifer","Zalika","Johnny","Dominique B","Kim D","Jeseekia","Tricia"};
+		String [] food = {"Salmon","Pizza","Tacos","Mexican","Chinese","Steak","Shrimp","Salmon","Pizza","Tacos","Italian","Pasta","Thai","Pizza","Ice cream","Lasagna","Mashed potatoes"};
+		String [] hometown = {"Detroit","Detroit","Monroe","Detroit","Hamtramack","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit"};
+	
+		
+		
+	for (int i = 0; i < names.length;i++);
+	{
+		System.out.println(food[4]);
+		
 	}
+	}
+	
 
 }
