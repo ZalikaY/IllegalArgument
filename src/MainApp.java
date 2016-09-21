d2fa8b36ddaf0ff19bbe21f819c8eebcd9537326
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-21 18:12:56 -0400

    Functional

--- src/MainApp.java
+++ src/MainApp.java
@@ -12,46 +12,54 @@ public class MainApp {
 		
 		//Useless variables atm
 		String userCont = "YES"; //Variable for user to continue
-		boolean cont = true;//Variable to control loop
 		int user = 0;
-		
+		String userS;
 		  		
 		
 		//Create parallel Arrays
 		String [] names = {"Armani","Yazmin", "Tyler","Nicole","Daniel","David","Dominique E", "Elle","Clark","Kevin","Jennifer","Zalika","Johnny","Dominique B","Kim D","Jeseekia","Tricia"};
 		String [] food = {"Salmon","Pizza","Tacos","Mexican","Chinese","Steak","Shrimp","Salmon","Pizza","Tacos","Italian","Pasta","Thai","Pizza","Ice cream","Lasagna","Mashed potatoes"};
 		String [] hometown = {"Detroit","Detroit","Monroe","Detroit","Hamtramack","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit","Detroit"};
-	
-		//While loop
-		while(userCont.equalsIgnoreCase("Yes"))
+		
+		
+		while (userCont.equalsIgnoreCase("Yes")){
 		
 		//Welcome user
+			try{
 		System.out.println("Welcome to our Java class.Which person would you like to learn more about?(enter a number 1-17): ");
 		user = userInt.nextInt();
+			} catch (InputMismatchException e){
+				System.out.println("That student does not exist.(Enter a number 1-17");
+			}
 		userInt.nextLine();
 		
-		try{//Try statement to catch array error
-			//Print out reference to array
+		
+		
+		//Try statement to catch array error
+			try {
 			System.out.println("Student " + user + " is " + names[user] +".What would you like to know about " + names[user] +(" enter hometown or favorite food)"));
-			} catch (ArrayIndexOutOfBoundsException e){
-		System.out.println("That student does not exist.Please try again(enter a number 1 - 17)");
-
+			} catch (ArrayIndexOutOfBoundsException r){
+				System.out.println("That student does not exist.Please try again(Enter a number 1 to 17");
+				userInt.nextInt();
 			}
-		String userS = userInt.next();
+		
+		userS = userInt.nextLine();
 		
 		if(userS.equalsIgnoreCase("hometown")){
 			System.out.println(names[user] + " is from " + hometown[user] + " Would you like to know more?");
+			userCont = userInt.nextLine();
 		} else if (userS.equalsIgnoreCase("favorite food")){
-			System.out.println(names[user] + " favorite food is " + food[user]);
-		}else { 
-			System.out.println("That data does not exist.Please try again");
+			System.out.print(names[user] + " 's favorite food is " + food[user] +"Would you like to know more?");
+			userCont = userInt.nextLine();
+		}else 	System.out.println("That does not exist");
 		}
 	
-	{
+		userS= userInt.nextLine();
+		System.out.println("Thanks");
+		
+		
+	}}
 	
-	}	
-	}
-	}
 	
 	
 
